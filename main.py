f8578d6a7ed2c56c8c55aacbdf2a5f7e0184f74b
Anthony Montes <anthony_montes4@my.vcccd.edu>
2024-06-26 20:05:00 -0700

    Lab 4-8

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
+def is_prime(num):
+    if num <= 1:
+        return False
+    if num == 2:
+        return True
+    if num % 2 == 0:
+        return False
+    
+    
+    prime_num = int(num ** 0.5) + 1
+    for i in range(3, prime_num, 2):
+        if num % i == 0:
+            return False
+    
+    return True
+
 def main():
     plist = []
-    ##################################################
-    # Comlete your code here
-    ##################################################
-
+    
+    begin = int(input("Enter integer 1: "))
+    end = int(input("Enter integer 2: "))
+    
+    for num in range(begin, end + 1):
+        if is_prime(num):
+            plist.append(num)
+    
+    print(plist)
     return plist
 
 
